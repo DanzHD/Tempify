8d532ab24e528b2a5bfa86b4fad94cab28de542b
Duongdh <danielhangsengduong@gmail.com>
2024-01-11 10:33:25 +1100

    Added pkce authentication

---
+++ src/pkce.jsx
@@ -0,0 +1,32 @@
+const generateRandomString = (length) => {
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const values = crypto.getRandomValues(new Uint8Array(length));
+    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+}
+const sha256 = async (plain) => {
+    const encoder = new TextEncoder()
+    const data = encoder.encode(plain)
+    return window.crypto.subtle.digest('SHA-256', data)
+}
+const base64encode = (input) => {
+    return btoa(String.fromCharCode(...new Uint8Array(input)))
+        .replace(/=/g, '')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_');
+}
+
+export const generateCodeVerifier = () => {
+    return generateRandomString(64);
+}
+
+
+
+export async function generateCodeChallengeFromVerifier(codeVerifier) {
+    const hashed = await sha256(codeVerifier);
+
+    return ase64encode(hashed);
+
+
+
+}
+
