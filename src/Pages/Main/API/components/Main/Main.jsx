95d0435790cd20806cdc749eb51674b273e0ac16
Duongdh <danielhangsengduong@gmail.com>
2024-01-16 18:48:33 +1100

    Set up tempo filtering

--- src/Pages/Main/API/components/Main/Main.jsx
+++ src/Pages/Main/API/components/Main/Main.jsx
@@ -1,9 +1,10 @@
 import './main.css'
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import Header from "../Header/Header.jsx";
 import Text from "../../../../../Common/Components/Text/Text.jsx";
 import Select from "react-select";
 import Button from "../../../../../Common/Components/Button/Button.jsx";
+import getItems from "../../getItems.js";
 
 const STAGES = {
     Select: 'Select BPM',
@@ -12,29 +13,42 @@ const STAGES = {
 }
 
 const TEMPO_OPTIONS = [
-    { value: 'Grave', label: '20-40'},
-    { value: 'lento', label: '40-60'},
-    { value: 'Adagio', label: '60-76'},
-    { value: 'Andante', label: '76-108'},
-    { value: 'Moderato', label: '108-120'},
-    { value: 'Allegro', label: '120-168'},
-    { value: 'Presto', label: '168-200'},
-    { value: 'Prestissimo', label: '200+'},
+    { value: '20-40', label: '20-40'},
+    { value: '40-60', label: '40-60'},
+    { value: '60-76', label: '60-76'},
+    { value: '76-108', label: '76-108'},
+    { value: '108-120', label: '108-120'},
+    { value: '120-168', label: '120-168'},
+    { value: '168-200', label: '168-200'},
+    { value: '200+', label: '200+'},
     { value: 'noTempo', label: 'Any'}
 
 ];
 
+
 export default function Main({ accessToken, signOut }) {
     const [stage, setStage] = useState(STAGES.Select);
-    const [tempo, setTempo] = useState(null);
-    // useEffect(() => {
-    //     getItems({ accessToken })
-    // }, [])
+    const [tracks, setTracks] = useState(null);
 
-    const handleTempoNext = (e) => {
+    const onGeneratePlaylist = async (e) => {
         e.preventDefault();
-        setStage(STAGES.Acoustic);
-        setTempo(e.target.tempo.value)
+        let retrievedTracks = await getItems({accessToken});
+        let tempo = e.target.tempo.value;
+        if (tempo === 'noTempo' || tempo === null) {
+            setTracks(retrievedTracks)
+            return;
+        }
+
+        if (tempo === '200+') {
+            retrievedTracks = retrievedTracks.filter(track => track.tempo >= 200);
+        } else {
+            let [lowerBound, upperBound] = tempo.split('-');
+            retrievedTracks = retrievedTracks.filter(track => (track.tempo >= lowerBound) && (track.tempo <= upperBound));
+
+        }
+        setTracks(retrievedTracks);
+        setStage(STAGES.Tailor);
+
     }
 
     return (
@@ -46,7 +60,7 @@ export default function Main({ accessToken, signOut }) {
                         (stage === STAGES.Select) &&
                             <>
                                 <Text subheading style={{fontWeight: '600'}}>Select the tempo of your new playlist</Text>
-                                <form style={{display: 'flex', alignItems: 'center', flexDirection: 'column'}} onSubmit={handleTempoNext}>
+                                <form style={{display: 'flex', alignItems: 'center', flexDirection: 'column'}} onSubmit={onGeneratePlaylist}>
                                     <Select name='tempo' className='tempo_select' options={TEMPO_OPTIONS} />
                                     <Button type='submit' style={{marginTop: '20px'}}>Create playlist</Button>
                                 </form>
