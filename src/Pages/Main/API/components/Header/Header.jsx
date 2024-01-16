f38e86cedf7a9b337985fcd13ef2ae8410b0f358
Duongdh <danielhangsengduong@gmail.com>
2024-01-16 17:34:44 +1100

    set up basics for select tempo page

--- src/Pages/Main/API/components/Header/Header.jsx
+++ src/Pages/Main/API/components/Header/Header.jsx
@@ -1,13 +1,15 @@
 import Button from "../../../../../Common/Components/Button/Button.jsx";
 import './header.css'
-export default function Header({logo, signOut}) {
+import Text from "../../../../../Common/Components/Text/Text.jsx";
+export default function Header({logo, description, signOut}) {
 
     return (
         <div className='header'>
             <div>{logo}</div>
+            <Text heading>{description}</Text>
             <Button onClick={signOut}>
                 <span className='material-symbols-outlined'>logout</span>
-                <div>Sign out </div>
+                <div>Sign out</div>
             </Button>
         </div>
     )
--- src/Pages/Main/API/components/Main/Main.jsx
+++ src/Pages/Main/API/components/Main/Main.jsx
@@ -1,23 +1,60 @@
 import './main.css'
 import {useState} from "react";
 import Header from "../Header/Header.jsx";
+import Text from "../../../../../Common/Components/Text/Text.jsx";
+import Select from "react-select";
+import Button from "../../../../../Common/Components/Button/Button.jsx";
 
 const STAGES = {
-    BPM: 'Select BPM',
-    Acoustic: 'Select Acoustic'
-
+    Select: 'Select BPM',
+    Tailor: "Tailor playlist",
+    Create: 'Playlist created'
 }
 
+const TEMPO_OPTIONS = [
+    { value: 'Grave', label: '20-40'},
+    { value: 'lento', label: '40-60'},
+    { value: 'Adagio', label: '60-76'},
+    { value: 'Andante', label: '76-108'},
+    { value: 'Moderato', label: '108-120'},
+    { value: 'Allegro', label: '120-168'},
+    { value: 'Presto', label: '168-200'},
+    { value: 'Prestissimo', label: '200+'},
+    { value: 'noTempo', label: 'Any'}
+
+];
+
 export default function Main({ accessToken, signOut }) {
-    const [stages, setStage] = useState(STAGES.BPM);
+    const [stage, setStage] = useState(STAGES.Select);
+    const [tempo, setTempo] = useState(null);
     // useEffect(() => {
     //     getItems({ accessToken })
     // }, [])
 
+    const handleTempoNext = (e) => {
+        e.preventDefault();
+        setStage(STAGES.Acoustic);
+        setTempo(e.target.tempo.value)
+    }
+
     return (
         <>
             <div className='main'>
-                <Header signOut={signOut} logo={<div className='logo'>SpotPlot</div>} />
+                <Header signOut={signOut} description={stage} logo={<div className='logo'>SpotPlot</div>} />
+                <div className='filter'>
+                    {
+                        (stage === STAGES.Select) &&
+                            <>
+                                <Text subheading style={{fontWeight: '600'}}>Select the tempo of your new playlist</Text>
+                                <form style={{display: 'flex', alignItems: 'center', flexDirection: 'column'}} onSubmit={handleTempoNext}>
+                                    <Select name='tempo' className='tempo_select' options={TEMPO_OPTIONS} />
+                                    <Button type='submit' style={{marginTop: '20px'}}>Create playlist</Button>
+                                </form>
+                            </>
+
+                    }
+                </div>
+
             </div>
         </>
     )
